fb52e0696a883499f8cbc56920018846ff062fe5
GemmaMorales <gemmaamorales@gmail.com>
2020-05-10 22:23:29 +0000

    create repository

---
+++ src/app.py
@@ -0,0 +1,62 @@
+import json
+from DataStructures import Queue
+from sms import send
+
+# there queue has to be declared globally (outside any other function)
+# that way all methods have access to it
+queue = Queue(mode="FIFO")
+    
+def print_queue():
+    # you must print on the console the entire queue list
+    print("Printing the entire list...")
+    
+    print(queue.get_queue())
+
+def add(name):
+    queue.enqueue(name)
+    return queue
+    
+
+def dequeue():
+    queue.dequeue()
+    return queue
+    
+
+def save():
+    pass
+
+def load():
+    pass 
+        
+    
+print("\nHello, this is the Command Line Interface for a Queue Managment application.")
+stop = False
+while stop == False:
+    
+    print('''
+What would you like to do (type a number and press Enter)?
+- Type 1: For adding someone to the Queue.
+- Type 2: For removing someone from the Queue.
+- Type 3: For printing the current Queue state.
+- Type 4: To export the queue to the queue.json file.
+- Type 5: To import the queue from the queue.json file.
+- Type 6: To quit
+    ''')
+
+    option = int(input("Enter a number:"))
+    # add your options here using conditionals (if)
+    if option == 3:
+        print_queue()
+    elif option == 6:
+        print("Bye bye!")
+        stop = True
+    elif option == 1:
+        name = input("Enter the name you want to add to the list:")
+        add(name)
+    elif option == 2:
+        dequeue()
+    elif option == 4:
+        
+    elif option == 5:
+    else:
+        print("Invalid option "+str(option))
